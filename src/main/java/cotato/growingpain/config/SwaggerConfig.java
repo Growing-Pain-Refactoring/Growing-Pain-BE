d2aff3b0f2af21e872d08ec9eff157b768bf966a
yunhacandy <yunhacandy@gmail.com>
2026-02-23 20:55:11 +0900

    refactor: 하드코딩 일부 삭제

--- src/main/java/cotato/growingpain/config/SwaggerConfig.java
+++ src/main/java/cotato/growingpain/config/SwaggerConfig.java
@@ -10,13 +10,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .addServersItem(new Server().url("http://43.201.210.211:8080").description("Dev Server"))
                 .addServersItem(new Server().url("http://localhost:8080").description("Local Server"))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components().addSecuritySchemes("bearerAuth",
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components().addSecuritySchemes(SECURITY_SCHEME_NAME,
                         new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
